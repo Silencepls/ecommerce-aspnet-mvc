ed4a1ebc284a8705a0cb022846d40e63b69850b2
Silencepls <viniciustorrescancela@gmail.com>
2023-04-10 16:00:05 -0300

    Models and DBContext

---
+++ Models/Author.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eBooks.Models
+{
+    public class Author
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProfilePictureURL { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eBooks.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageURL { get; set; }
+        public double Price { get; set; }
+        public DateTime Year_of_Edition { get; set; }
+    }
+}
